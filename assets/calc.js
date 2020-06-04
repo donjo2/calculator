a4c3cf688a469f7bfa2d0d2e7841090ad8e40d0f
donjo2 <jo4christ48@yahoo.com>
2020-06-04 07:53:28 +0100

    First push
    Basic sketch, layout and CSS Grid templating

---
+++ assets/calc.js
@@ -0,0 +1,15 @@
+(function(){
+	"use strict";
+	//declare global variables
+
+
+	//core functions
+	
+
+	//utility functions
+	//1. addVal() - add val to screen
+	//2. toggleClass() - toggles element's class
+
+
+	//event listeners
+}());
